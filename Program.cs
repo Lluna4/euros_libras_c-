42974d0231220bae761df8313fda6d9f235aa8c5
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-07-12 02:05:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Euros o Libras?");
+           string t = Console.ReadLine();
+
+           static void test()
+           {
+               Console.WriteLine("Funciona....");
+           }
+
+           test();
+
+           if (t == "Euros")
+           {
+               Console.WriteLine("Cuantas libras tienes?");
+               string libras = Console.ReadLine();
+               float lib = float.Parse(libras);
+               Console.WriteLine(String.Format("Tienes {0} Euros", lib * 1.17));
+               
+
+
+           }
+           if (t == "Libras")
+           {
+                Console.WriteLine("Cuantos euros tienes?");
+               string euros = Console.ReadLine();
+               float eur = float.Parse(euros);
+               Console.WriteLine(String.Format("Tienes {0} Libras", eur * 0.83)); 
+           }
+           
+
+        }
+    }
+}
